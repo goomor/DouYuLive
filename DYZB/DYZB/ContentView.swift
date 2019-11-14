e493dadee2371a2612866f72f94b53d0b17c85d9
LAC <goomor@yeah.net>
2019-11-14 11:53:14 +0800

    初始化项目

---
+++ DYZB/DYZB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DYZB
+//
+//  Created by LAC on 2019/11/14.
+//  Copyright © 2019 LAC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
